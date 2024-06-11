0fed468a11cc83b25a3862f2d9e9b355400a3369
mike123691 <duymai442@gmail.com>
2024-06-12 00:50:51 +0700

    Create Product Services

---
+++ ASM_Nhom2_API/Service/ProductServices/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ASM_Nhom2_API.Data;
+using ASM_Nhom2_API.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ASM_Nhom2_API.Service.ProductServices
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVM>> GetAllProductsAsync();
+        Task<ProductVM> GetProductByIdAsync(int productId);
+        Task AddProductAsync(ProductVM productVM);
+        Task UpdateProductAsync(int productId, ProductVM productVM);
+        Task DeleteProductAsync(int productId);
+    }
+
+}
