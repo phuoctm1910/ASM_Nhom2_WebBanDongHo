f18c6439e7c7d21be3a972a5bb087dfcc8579ae8
phuoctm1910 <nothinginthis465@gmail.com>
2024-06-13 01:19:23 +0700

    update route admin

--- ASM_Nhom2_View/Startup.cs
+++ ASM_Nhom2_View/Startup.cs
@@ -48,11 +48,12 @@ namespace ASM_Nhom2_View
             {
                 endpoints.MapControllerRoute(
                     name: "areas",
-                    pattern: "{area:exists}/{controller=HomeAdmin}/{action=Index}/{id?}");
-
+                    pattern: "{area:exists}/{controller=HomeAdmin}/{action=Index}/{id?}"
+                );
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
 
