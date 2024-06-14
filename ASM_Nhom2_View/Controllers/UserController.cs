247815eace779235f7c6f558b720fceca793f7f4
mike123691 <duymai442@gmail.com>
2024-06-14 16:28:01 +0700

    update-database & authenticator

--- ASM_Nhom2_View/Controllers/UserController.cs
+++ ASM_Nhom2_View/Controllers/UserController.cs
@@ -1,15 +1,16 @@
 ﻿using ASM_Nhom2_View.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASM_Nhom2_View.Controllers
 {
     public class UserController : Controller
     {
+
         private readonly AppDbContext _context;
 
         public UserController(AppDbContext context)
@@ -17,11 +18,51 @@ namespace ASM_Nhom2_View.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(User user)
+        {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                var u = _context.Users.Where(x => x.UserName.Equals(user.UserName) && x.Password.Equals(user.Password)).FirstOrDefault();
+                if (u != null)
+                {
+                    HttpContext.Session.SetString("UserName", u.UserName.ToString());
+                    HttpContext.Session.SetString("Password", u.Password.ToString());
+                    HttpContext.Session.SetString("FullName", u.FullName.ToString());
+                    if (u.RoleId == 2)
+                    {
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "HomeAdmin", new { area = "Admin" });
+
+                    }
+                }
+            }
             return View();
         }
+        [ActionName("Logout")]
         [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
         public IActionResult Register()
         {
             var genderSelectList = new SelectList(new List<SelectListItem>
--- ASM_Nhom2_View/Startup.cs
+++ ASM_Nhom2_View/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ASM_Nhom2_View
 {
@@ -19,42 +20,44 @@ namespace ASM_Nhom2_View
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add services to the container.
             services.AddControllersWithViews();
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
-            services.AddHttpClient();
-
+            services.AddDbContext<AppDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+            services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromSeconds(1000);
+                option.Cookie.HttpOnly = true;
+                option.Cookie.IsEssential = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            // Configure the HTTP request pipeline.
+            if (!env.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "areas",
-                    pattern: "{area:exists}/{controller=HomeAdmin}/{action=Index}/{id?}");
-
+                    pattern: "{area:exists}/{controller=HomeAdmin}/{action=Index}/{id?}"
+                );
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
-
     }
 }
