7e3d6718fd9fd5a6a461a9d737876bced0d478fd
mike123691 <duymai442@gmail.com>
2024-06-14 16:28:01 +0700

    update-database & authenticator

--- ASM_Nhom2_View/Startup.cs
+++ ASM_Nhom2_View/Startup.cs
@@ -1,10 +1,11 @@
+using ASM_Nhom2_View.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ASM_Nhom2_View.Data;
+using System;
 
 namespace ASM_Nhom2_View
 {
@@ -19,8 +20,14 @@ namespace ASM_Nhom2_View
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add services to the container.
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+            services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromSeconds(1000);
+                option.Cookie.HttpOnly = true;
+                option.Cookie.IsEssential = true;
+            });
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
             services.AddHttpClient();
@@ -37,32 +44,32 @@ namespace ASM_Nhom2_View
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            // Configure the HTTP request pipeline.
+            if (!env.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "areas",
-                    pattern: "{area:exists}/{controller=HomeAdmin}/{action=Index}/{id?}");
-
+                    pattern: "{area:exists}/{controller=HomeAdmin}/{action=Index}/{id?}"
+                );
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
-
     }
 }
