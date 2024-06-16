1354b52aaec5666908bbe34270933eba59a2e135
minh <haudmpc06680@fpt.edu.vn>
2024-06-16 11:09:44 +0700

    Product Details 16/6

--- ASM_Nhom2_View/Controllers/ProductDetailController.cs
+++ ASM_Nhom2_View/Controllers/ProductDetailController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASM_Nhom2_View.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ASM_Nhom2_View.Controllers
 {
     public class ProductDetailController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public ProductDetailController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Brand) // Ensure Brand is included
+                .FirstOrDefaultAsync(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product); // Đảm bảo bạn truyền đúng đối tượng vào view
         }
     }
 }
