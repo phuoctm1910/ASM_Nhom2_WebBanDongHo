10890464f3bf61de3ea2e751da63aac118a0afd4
hieuphpc07025 <144969739+hieuphpc07025@users.noreply.github.com>
2024-06-18 17:03:25 +0700

    changeinfo controller

--- ASM_Nhom2_View/Controllers/UserController.cs
+++ ASM_Nhom2_View/Controllers/UserController.cs
@@ -1,10 +1,18 @@
-﻿using ASM_Nhom2_View.Data;
+﻿using ASM_Nhom2_View.Models;
+using ASM_Nhom2_View.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.AspNetCore.Hosting;
+
 
 namespace ASM_Nhom2_View.Controllers
 {
@@ -12,10 +20,12 @@ namespace ASM_Nhom2_View.Controllers
     {
 
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _web;
 
-        public UserController(AppDbContext context)
+        public UserController(AppDbContext context, IWebHostEnvironment web)
         {
             _context = context;
+            _web = web;
         }
 
         [HttpGet]
@@ -77,14 +87,6 @@ namespace ASM_Nhom2_View.Controllers
             return View();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Changeinfo()
-        {
-            return View();
-        }
-
-
-
 
         [HttpPost]
         public async Task<IActionResult> Register([Bind("FullName,Gender,PhoneNumber,UserName,Password,BirthDate")] User user)
@@ -115,7 +117,83 @@ namespace ASM_Nhom2_View.Controllers
 
         }
 
-        
+        [HttpGet]
+        public async Task<IActionResult> Changeinfo()
+        {
+            var userName = HttpContext.Session.GetString("UserName");
+
+            if (userName == null)
+            {
+                return View();
+            }
+
+            // Retrieve the user from the database
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Pass the user data to the view
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Changeinfo(User change, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                var userName = HttpContext.Session.GetString("UserName");
+
+                if (userName == null)
+                {
+                    return View();
+                }
+
+                var userToUpdateInfo = await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+
+                if (userToUpdateInfo == null)
+                {
+                    return NotFound();
+                }
+
+                userToUpdateInfo.UserName = change.UserName;
+                userToUpdateInfo.FullName = change.FullName;
+                userToUpdateInfo.PhoneNumber = change.PhoneNumber;
+                userToUpdateInfo.Email = change.Email;
+                userToUpdateInfo.Gender = change.Gender;
+                userToUpdateInfo.BirthDate = change.BirthDate;
+                if (file != null && file.Length > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var filePath = Path.Combine(_web.WebRootPath, "uploads", fileName);
+
+                    if (!System.IO.File.Exists(filePath))
+                    {
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                    }
+                    userToUpdateInfo.Image = fileName;
+                }
+                _context.Users.Update(userToUpdateInfo);
+                await _context.SaveChangesAsync();
+
+                HttpContext.Session.SetString("FullName", userToUpdateInfo.FullName);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Cập nhật tài khoản thất bại");
+            }
+
+            return View(change);
+        }
+
+
 
 
     }
